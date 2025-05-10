e55306a50672f6ce7069b93c459063e918a40cf5
Areeshakhan723 <areshakhan6400@gmail.com>
2025-05-10 13:48:01 +0500

    some changes

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,13 +1,17 @@
 // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity";
-import { client } from './client'
 
-export const { sanityFetch, SanityLive } = defineLive({ 
-  client: client.withConfig({ 
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX' 
-  }) 
-});
+import { SanityClient } from "@sanity/client";
+
+// Query function with proper types
+export const sanityFetch = async <T>(
+  query: string,
+  params: Record<string, unknown> = {},
+  client: SanityClient
+): Promise<T> => {
+  return await client.fetch<T>(query, params);
+};
+
+// Dummy component to avoid errors if imported
+export const SanityLive = () => null;
